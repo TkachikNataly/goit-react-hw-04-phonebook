abf46ad2cabfd600b5e969f253f659a76c0610d3
TkachikNataly <tkachik.nataly@gmail.com>
2022-06-02 01:54:40 +0300

    hooks

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,64 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import s from './Form.module.css';
 import PropTypes from 'prop-types';
 
-
-class Form extends Component {
-    state = {
-        name: '',
-        number: '',
+export default function Form({ onFormSubmit }) {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+    const reset = () => {
+        setName('');
+        setNumber('');
     };
-
-    handleSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
 
-        this.props.onFormSubmit(this.state);
-        this.resetForm();
+        onFormSubmit({ name, number });
+        reset();
+    };
+    const handleChangeName = e => {
+        setName(e.target.value);
     };
 
-    handleChange = e => {
-        const { name, value } = e.currentTarget;
-        this.setState({ [name]: value });
+    const handleChangeNumber = e => {
+        setName(e.target.value);
     };
-
-    resetForm = () => {
-        this.setState({ contacts: [], name: '', number: '' });
-    };
-
-    render = () => {
-        return (
-            <div>
-                <form className={s.form} onSubmit={this.handleSubmit}>
-                    <p className={s.title}>Name</p>
-                    <input
-                        className={s.input}
-                        type="text"
-                        name="name"
-                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                        required
-                        value={this.state.name}
-                        onChange={this.handleChange}
-                    />
-                    <p className={s.title}>Number</p>
-                    <input
-                        className={s.input}
-                        type="tel"
-                        name="number"
-                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                        required
-                        value={this.state.number}
-                        onChange={this.handleChange}
-                    />
-                    <button className={s.button} type="submit">Add Contact</button>
-                </form>
-            </div>
-        );
-    };
+    return (
+        <div>
+            <form className={s.form} onSubmit={handleSubmit}>
+                <p className={s.title}>Name</p>
+                <input
+                    className={s.input}
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                    value={name}
+                    onChange={handleChangeName}
+                />
+                <p className={s.title}>Number</p>
+                <input
+                    className={s.input}
+                    type="tel"
+                    name="number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                    value={number}
+                    onChange={handleChangeNumber}
+                />
+                <button className={s.button} type="submit">Add Contact</button>
+            </form>
+        </div>
+    );
 }
 Form.propTypes = {
     onFormSubmit: PropTypes.func.isRequired,
 };
-
-export default Form;
